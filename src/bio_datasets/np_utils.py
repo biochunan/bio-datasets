477e18252707bdfafc22670020ab591234e10a53
alex-hh <alex.hhooker@gmail.com>
2024-10-17 17:10:23 +0100

    add np_utils module

---
+++ src/bio_datasets/np_utils.py
@@ -0,0 +1,18 @@
+import numpy as np
+from typing import List
+
+
+def map_categories_to_indices(arr: np.ndarray, categories: List[str]) -> np.ndarray:
+    """
+    Map categories to indices.
+
+    Args:
+        arr (np.ndarray): The input array of categories.
+        categories (List[str]): The list of categories to map to indices.
+
+    Returns:
+        np.ndarray: An array of indices corresponding to the categories.
+    """
+    unique_categories, indices = np.unique(arr, return_inverse=True)
+    category_indices = np.array([categories.index(i) for i in unique_categories])
+    return category_indices[indices]
